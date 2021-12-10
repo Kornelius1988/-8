41188e223d422581465b9f1e5db511220fa28985
Андрей Блинников <kornelius1988@gmail.com>
2021-12-10 18:02:55 +0300

    1

---
+++ File/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace File
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "Logs";
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+        }
+    }
+}
---
+++ File2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace File2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "Logs2/log.txt";
+
+            // Создание папки
+            //DirectoryInfo directory = new DirectoryInfo(path);
+            //if (!directory.Exists)
+            //{
+            //    directory.Create();
+            //}
+
+            // Создание *.txt
+            //if (!File.Exists(path))
+            //{
+            //    File.Create(path);
+            //}
+            using (StreamWriter sw = new StreamWriter(path, true))
+            {
+                sw.WriteLine("Старт программы");
+                sw.WriteLine("Ошибка");
+            }
+            using (StreamReader sr = new StreamReader(path))
+            {
+                Console.WriteLine(sr.ReadToEnd());
+            }
+            Console.ReadKey();
+        }
+    }
+}
